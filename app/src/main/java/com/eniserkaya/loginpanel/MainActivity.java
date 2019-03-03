12da79ece604ded3d253c83dcf34f38dd3ec9abd
=eniserkaya <=aeniserkaya@gmail.com>
2019-03-03 15:55:07 +0300

    tasarim iyilestirme

--- app/src/main/java/com/eniserkaya/loginpanel/MainActivity.java
+++ app/src/main/java/com/eniserkaya/loginpanel/MainActivity.java
@@ -2,14 +2,30 @@ package com.eniserkaya.loginpanel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button tiklaBtn;
+    private TextView textYazisiTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tiklaBtn = findViewById(R.id.first_btn_id);
+
+
+        tiklaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "TIKLADIN!", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         //Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
     }
